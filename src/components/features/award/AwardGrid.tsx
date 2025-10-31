d6acbb88d8380fef78cafe530d6ef0464d6d25a3
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-11-01 01:40:28 +0900

    🚧 Award

---
+++ src/components/features/award/AwardGrid.tsx
@@ -0,0 +1,12 @@
+import { cva } from "styled-system/css"
+import { styled as s } from "styled-system/jsx"
+
+export const awardGridRecipes = cva({
+  base: {
+    display: "grid",
+    gridTemplateColumns: "repeat(1, minmax(0, 1fr))",
+    gap: "6",
+  },
+})
+
+export const AwardGrid = s("div", awardGridRecipes);
