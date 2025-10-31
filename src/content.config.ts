803ed70e9b92bb93ea324d21791e25883bf5fb77
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-11-01 01:38:14 +0900

    ✨ isPublish機能の追加

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,53 +1,45 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
+// 共通のスキーマ
+const commonSchema = z.object({
+  title: z.string(),
+  author: z.string().optional(),
+  slug: z.string(),
+  description: z.string().optional(),
+  isPublish: z.boolean().default(false),
+})
+
 // blog コレクション
 const blogCollection = defineCollection({
   loader: glob({ base: './src/content/blog', pattern: '**/*.md' }),
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      slug: z.string().optional(),
-      author: z.string().optional(),
-      description: z.string(),
-      // 日付を Date オブジェクトに変換
-      publishDate: z.coerce.date(),
-      updateDate: z.coerce.date().optional(),
-      tags: z.array(z.string()).optional(),
-      emoji: z.string().optional(),
-      category: z.string().optional(),
-    }),
+  schema: commonSchema.extend({
+    publishDate: z.coerce.date(),
+    updateDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).optional(),
+    emoji: z.string().optional(),
+    category: z.string().optional(),
+  }),
 });
 
 // product コレクション
 const productCollection = defineCollection({
   loader: glob({ base: './src/content/product', pattern: '**/*.md' }),
-
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      slug: z.string().optional(),
-      author: z.string().optional(),
-      publishDate: z.coerce.date(),
-      updateDate: z.coerce.date().optional(),
-      tags: z.array(z.string()).optional(),
-      thumbnail: z.string(),
-    })
+  schema: commonSchema.extend({
+    publishDate: z.coerce.date(),
+    updateDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).optional(),
+    thumbnail: z.string(),
+  }),
 })
 
 // Award コレクション
 const awardCollection = defineCollection({
   loader: glob({ base: './src/content/award', pattern: '**/*.md' }),
-
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      slug: z.string().optional(),
-      type: z.string(),
-      description: z.string(),
-      date: z.coerce.date(),
-    })
+  schema: commonSchema.extend({
+    type: z.string(),
+    date: z.coerce.date(),
+  }),
 })
 
 // コレクションをエクスポート
