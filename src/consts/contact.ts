45c53ef1001b5f211a03f2637c42c93be8f9c690
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-11-03 02:15:56 +0900

    ⚡️ パフォーマンスの改善 - headingのレベル調整 - aタグにlabelを追加 - sitemapのなにか(よくわかってない)

--- src/consts/contact.ts
+++ src/consts/contact.ts
@@ -2,19 +2,23 @@ import type { ContactLink } from "@/types";
 
 export const CONTACT_LINKS: ContactLink[] =[
   {
+    label: "Twitter",
     url: "https://twitter.com/suzuuuuu_09",
-    icon: "logos:twitter"
+    icon: "logos:twitter",
   },
   {
+    label: "GitHub",
     url: "https://github.com/suzuuuuu09",
     icon: "logos:github-icon"
   },
   {
+    label: "Zenn",
     url: "https://zenn.dev/suzuuuuu09",
     icon: "simple-icons:zenn",
     color: "#3EA8FF"
   },
   {
+    label: "Qiita",
     url: "https://qiita.com/suzuuuuu09",
     icon: "simple-icons:qiita",
     color: "#55C500"
