18a98ea1905a70561d6f67f70ea638a4b92a383c
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-11-05 13:44:03 +0900

    ✨ OGをcollection指定してできるように変更

---
+++ src/pages/api/og/[collection]/[title].png.ts
@@ -0,0 +1,36 @@
+import type { APIRoute, APIContext } from "astro";
+import { getCollection } from "astro:content";
+import { generateOgImage } from "../../../../components/features/og/OgImage";
+
+const COLLECTIONS = ["blog", "award", "product"] as const;
+
+export const getStaticPaths = async () => {
+  // COLLECTIONを動的に取得
+  const paths = [];
+  for (const collection of COLLECTIONS) {
+    let posts = await getCollection(collection);
+    posts = posts.filter((post) => post.data.isPublish);
+
+    paths.push(
+      ...posts.map((post) => ({
+        params: { collection, title: post.data.slug },
+      }))
+    );
+  }
+  return paths;
+}
+
+export const GET: APIRoute = async ({ params }: APIContext) => {
+  // COLLECTIONを動的に取得
+  const posts = await getCollection(params.collection as typeof COLLECTIONS[number]);
+  const filteredPosts = posts.filter((post) => post.data.isPublish);
+  const post = filteredPosts.find((post) => post.data.slug === params.title);
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  const image = await generateOgImage(post.data.title);
+  return new Response(image instanceof Buffer ? new Uint8Array(image) : image, {
+    headers: { "Content-Type": "image/png" },
+  });
+}
