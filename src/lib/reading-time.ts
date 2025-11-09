d8b89c12c64986149690f47ab63b14bfe3bf023c
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-11-10 00:51:01 +0900

    🩹 読了時間の計算にwikiリンクを除く

--- src/lib/reading-time.ts
+++ src/lib/reading-time.ts
@@ -5,7 +5,10 @@ import { toString } from 'mdast-util-to-string';
 export const getReadingTime = (text: string): string | undefined => {
   if (!text?.length) return undefined;
   try {
-    const { minutes } = calculateReadingTime(toString(fromMarkdown(text)));
+    // ![[...]] 形式のWikiリンクは計算しない
+    const cleanedText = text.replaceAll(/!\[\[.*?\]\]/g, '');
+    
+    const { minutes } = calculateReadingTime(toString(fromMarkdown(cleanedText)));
     if (minutes && minutes > 0) {
       return `${Math.ceil(minutes)} min`;
     }
