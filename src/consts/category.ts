e1bf340adf006b4fca50378a631ad0b5771bbbca
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-01-09 01:48:47 +0900

    ✨ あけおめ！
    2025→2026

--- src/consts/category.ts
+++ src/consts/category.ts
@@ -4,4 +4,5 @@ export const categories: CategoryItem[] = [
   { category: "private", emoji: "⭐️" },
   { category: "tech", emoji: "🖥️" },
   { category: "idea", emoji: "💡" },
+  { category: "work", emoji: "💼" },
 ]
