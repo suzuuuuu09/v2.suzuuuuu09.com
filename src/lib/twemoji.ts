13ef91d6365d55c8d4a7af74aa7fcf92d384d09a
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-01-29 15:56:56 +0900

    ⚡️パフォーマンスの改善

--- src/lib/twemoji.ts
+++ src/lib/twemoji.ts
@@ -2,8 +2,15 @@ import twemoji from "twemoji";
 
 // Twemojiの初期化
 export function initTwemoji() {
-  twemoji.parse(document.body, {
-    folder: "svg",
-    ext: ".svg",
-  });
+	twemoji.parse(document.body, {
+		folder: "svg",
+		ext: ".svg",
+		attributes: () => {
+			return {
+				loading: "lazy",
+				decoding: "async",
+			};
+		},
+	});
 }
+
